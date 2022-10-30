d53575f859d6bc36e0963633f241075cce246da2
Maksim Frankov <2m.franko@gmail.com>
2022-10-30 13:31:31 +0200

    2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = addItemCard(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function addItemCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+    `;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
